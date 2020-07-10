39d2eee987e58a061ea5b6a8ca112752ac71316c
Ace Fox <afox09@gmail.com>
2020-07-10 16:34:17 -0500

    initial

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,12 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/AvatarCosplay.jpg', description: 'The summer after college, my friends and I made all the costumes from Avatar', likes:0},
+    { id: 3, path: 'images/Banked.jpg', description: 'A banked track derby game I played at rollercon 2019', likes: 0 },
+    { id: 4, path: 'images/derbyWedding.jpg', description: 'My derby weddding with my now real-wife, Robin', likes: 0 },
+    { id: 5, path: 'images/firstAniversary.jpg', description: 'First aniversary with Robin, surrey at Minnehaha falls', likes: 0},
+    { id: 6, path: 'images/gay.jpg', description: 'Rockits WIN the Golden Skate 2020', likes: 0 },
+    { id: 7, path: 'StaceysWedding.jpg', description: 'My best friend, Staceys wedding', likes: 0 }
+
 ];
 
 module.exports = galleryItems;
