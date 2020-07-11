7ed9e4eb2bf96956c0ac7b0d05dd31547c63228e
Ace Fox <afox09@gmail.com>
2020-07-10 20:28:28 -0500

    clickable photos/ conditional render

--- src/components/galleryItem/galleryItem.js
+++ src/components/galleryItem/galleryItem.js
@@ -3,18 +3,40 @@ import './galleryItem.css'
 
 class GalleryItem extends Component {
 
+    state = {
+        clicked: true,
+    }//end state
+
     handleLike = (event) => {
         console.log('you liked photo #', this.props.thisItem.id );
-    }
+    }//end handleLike
+
+    handleCLick = (event) => {
+        console.log('in handleClick');
+        this.setState({
+            clicked:!this.state.clicked
+        })//end setState
+    }//end handleClick
 
 
     render() {
+        let photo;
+        if (this.state.clicked){
+            photo = 
+            <img src={this.props.thisItem.path} alt = {this.props.thisItem.description}
+            onClick = {this.handleCLick}/>
+        }else{
+            photo = <h3 onClick = {this.handleCLick}>{this.props.thisItem.description}</h3>;
+        }
+
       return (
         <div className='galleryItem'>
             
-        <img src={this.props.thisItem.path} alt = {this.props.thisItem.description}/>
+        {photo}
         <div className = "desc">
-            <button onClick = {this.handleLike} >like this photo</button>
+            <button onClick = {this.handleLike} >:
+            
+            like this photo</button>
              <p>Likes: {this.props.thisItem.likes}</p>
         </div>
         </div>
